d06205a6371a5c7129b3c5fd225d4dfb51ee872d
nicolas rolland <nicolas.roland@gmail.com>
2018-11-27 12:15:50 +0100

    adding tls settings

--- https-client/Main.hs
+++ https-client/Main.hs
@@ -1,22 +1,27 @@
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
-import           Control.Monad.Trans.Except (ExceptT, runExceptT)
-import           Data.Proxy
-import           Data.Text                  (Text)
-import qualified Data.Text                  as T
-import           HelloApi
-import           Network.HTTP.Client        (Manager, newManager)
-import           Network.HTTP.Client.TLS    (tlsManagerSettings)
-import           Servant.API
+import           Network.HTTP.Client.TLS    (tlsManagerSettings, mkManagerSettings)
 import           Servant.Client
 import           SimpleApi
+import qualified Network.Connection as NC (TLSSettings(..))
 
+import           Network.HTTP.Client        (newManager)
+import qualified Data.Text.IO as T
 
 main :: IO ()
 main = do
-    manager <- newManager tlsManagerSettings
+    stgs <- do
+      choice <- do
+        T.putStrLn "1. try with normal certificate check \n2. try with no check"
+        T.getLine
+      return $ case choice of
+        "2" ->  (mkManagerSettings (NC.TLSSettingsSimple True False False) Nothing)
+        "1" ->  tlsManagerSettings
+
+    manager <- newManager stgs
     let baseUrl = BaseUrl Https "localhost" 8080 ""
     res <- queries manager baseUrl
     case res of
