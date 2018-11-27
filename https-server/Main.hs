b38b46acc790397193872a829f7855bce83a3762
nicolas rolland <nicolas.roland@gmail.com>
2018-11-27 12:15:50 +0100

    adding tls settings

--- https-server/Main.hs
+++ https-server/Main.hs
@@ -10,6 +10,12 @@ import           Servant
 import           Servant.API
 import           SimpleApi
 
+ --  openssl genrsa -des3 -out server.key 1024
+ --  openssl req -new -key server.key -out server.csr
+ --  mv server.key server.key.org
+ --  openssl rsa -in server.key.org -out server.key
+ --  openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+
 main :: IO ()
 main = do
     runTLS (tlsSettings "server.crt" "server.key") (setPort 8080 defaultSettings) app
